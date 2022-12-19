cf56dc42d6693373b766746811c32bdabdca02db
unknown <simonchevalier112@gmail.com>
2022-12-19 16:36:53 +0100

    Fix for the guns after the Jato modif

--- DCS-NoShootOrKickGameGUI.lua
+++ DCS-NoShootOrKickGameGUI.lua
@@ -9,30 +9,33 @@ local onlinePlayer = nil
 local currentUnit = nil
 local lastShot = nil
 local posit = nil
+local needToBeIgnored = false
 
 function eventListiner:onEvent(event)
 	if event.id == 1 or event.id == 23 then
-		if event.weapon:getTypeName() ~= "SPRD-99" then
-			--trigger.action.outText("Pouet",1)
-			initiatorName = event.initiator:getPlayerName()
-			if(lastShot ~= event.time) then
-				lastShot = event.time
-				posit = event.initiator:getPoint()
-				if(net ~= nil and net.get_player_list() ~= nil and mist ~= nil) then
-					onlinePlayers = net.get_player_list()
-					units = mist.getUnitsInZones(blueUnits, zone_names)
-					if(initiatorName ~= nil and onlinePlayers ~= nil and units ~= nil and next(units) ~= nil) then
-						for i = 1, #units do 
-							currentUnit = units[i]
-							if (Unit ~= nil and currentUnit ~= nil and Unit.getPlayerName(currentUnit) ~= nil and Unit.getPlayerName(currentUnit) == initiatorName ) then -- Checking if player and if alive
-								for j = 1, #onlinePlayers do	
-									if ( onlinePlayers ~= nil and onlinePlayers[j] ~= nil ) then
-										onlinePlayer = onlinePlayers[j]
-										if(net.get_player_info(onlinePlayer, "name") == initiatorName) then
-											trigger.action.outTextForUnit(currentUnit:getID() , "" .. initiatorName ..", you were not allowed to shoot close to blue bases! (inside the zone with blue dots borders if you check the F10 map)\nYou have been shotdown!", 10)
-											net.send_chat(""..initiatorName.." has been shotdown!(No shoot close to blue airfields!)", true)
-											trigger.action.explosion(posit , 4) --Higher is this number, bigger is the explosion 💥
-										end
+		if event.weapon ~= nil and event.weapon:getTypeName() == "SPRD-99" then
+			needToBeIgnored = true
+			--trigger.action.outText("kaboum",1)
+		end
+		--trigger.action.outText(""..event.weapon:getTypeName().."",11)
+		initiatorName = event.initiator:getPlayerName()
+		if(lastShot ~= event.time and needToBeIgnored == false) then
+			lastShot = event.time
+			posit = event.initiator:getPoint()
+			if(net ~= nil and net.get_player_list() ~= nil and mist ~= nil) then
+				onlinePlayers = net.get_player_list()
+				units = mist.getUnitsInZones(blueUnits, zone_names)
+				if(initiatorName ~= nil and onlinePlayers ~= nil and units ~= nil and next(units) ~= nil) then
+					for i = 1, #units do 
+						currentUnit = units[i]
+						if (Unit ~= nil and currentUnit ~= nil and Unit.getPlayerName(currentUnit) ~= nil and Unit.getPlayerName(currentUnit) == initiatorName ) then -- Checking if player and if alive
+							for j = 1, #onlinePlayers do	
+								if ( onlinePlayers ~= nil and onlinePlayers[j] ~= nil ) then
+									onlinePlayer = onlinePlayers[j]
+									if(net.get_player_info(onlinePlayer, "name") == initiatorName) then
+										trigger.action.outTextForUnit(currentUnit:getID() , "" .. initiatorName ..", you were not allowed to shoot close to blue bases! (inside the zone with blue dots borders if you check the F10 map)\nYou have been shotdown!", 10)
+										net.send_chat(""..initiatorName.." has been shotdown!(No shoot close to blue airfields!)", true)
+										trigger.action.explosion(posit , 4) --Higher is this number, bigger is the explosion 💥
 									end
 								end
 							end
@@ -46,6 +49,7 @@ function eventListiner:onEvent(event)
 		initiatorName = nil
 		units = nil
 		currentUnit = nil
+		needToBeIgnored = false
 	end
 end
 world.addEventHandler(eventListiner)
